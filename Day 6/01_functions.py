1d508b15b1123a0c4fa6542ae0d606855b1c941f
Murtaza Tinwala <murtazatinwala1511@gmail.com>
2023-06-26 20:30:22 +0530

    functions , indentation , while , reeborg website

---
+++ Day 6/01_functions.py
@@ -0,0 +1,12 @@
+# Functions reduce the lines of code and makes our code more readable
+# If will get executed only when it will be called
+def greetings():
+    print("Hello")
+    print('Bye')
+greetings()
+# We can reeborg's keyboard to learn python functions
+# Note:- Indentation is very important in python and there is a very
+# big debate on that and stack overflow says average salary of 
+# programmers using space is more than using tabs and python also 
+# tells to use space and doesn't allows space and tabs simultaneously
+# in same code file
---
+++ Day 6/02_while_loop.py
@@ -0,0 +1,6 @@
+# while something_is_true:
+# Will become infinite loop when condition is always true
+i = 100
+while i > 0:
+    print(i)
+    i -= 1
