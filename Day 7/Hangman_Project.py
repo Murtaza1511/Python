6ffa6f2c77f7517c3ac67cb447f7d6d289207588
Murtaza Tinwala <murtazatinwala1511@gmail.com>
2023-06-27 11:52:35 +0530

    clear screen function added

--- Day 7/Hangman_Project.py
+++ Day 7/Hangman_Project.py
@@ -1,6 +1,8 @@
 #Step 5
 
 import random
+
+from click import clear
 import hangman_art
 import hangman_words
 
@@ -24,6 +26,7 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    clear() # It will clear screen after every guess
 
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
